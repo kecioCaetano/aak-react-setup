f95d8b61179e2d3fa109ee9f4e75be3c70fdbc24
Kecio Caetano Barbosa <kecio.kecio@gmail.com>
2026-04-05 09:27:38 -0400

    setting up new react project

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+// import './index.css'
 import App from './App.jsx'
-import Greeting from './Greeting.jsx'
-import TodoList from './DateTest.jsx'
-import AnimalList from './AnimalList.jsx'
-import AppAnimal from './props.jsx'
-import ListPeople from './people.jsx'
-import AppButtonFunc from './propsFunctions.jsx'
-import Gallery from './gallery.jsx'
-import Clock from './clock.jsx'
-import Form from './sendHi.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Form />
+    <App />
   </StrictMode>,
 )
